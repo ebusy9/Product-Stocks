b6cef0da0cd1bf2ea6af97b00ca1128726a55735
ebusy9 <g404ebus@gmail.com>
2023-11-27 15:33:08 +0100

    maj

--- src/App.jsx
+++ src/App.jsx
@@ -2,62 +2,63 @@ import { useState } from "react"
 import './assets/style.css'
 
 
-
-
-function searchBar() {
-
-  return <div className="search-bar">
-    <div className="input-wrapper">
-      <input type="text" name="search" id="search-input" placeholder="Rechercher" />
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <div className="search-input-wrapper">
+        <Input />
+      </div>
+      <div className="checkbox-wrapper">
+        <Checkbox />
+      </div>
     </div>
-    <div className="input-wrapper">
-      <input type="checkbox" name="isInStock" id="is-in-stock-checkbox" />
-      <label htmlFor="isInStock">N'afficher que les produits en stock</label>
+  )
+}
+
+function Input() {
+
+  return <input type="text" name="search" id="search-input" placeholder="Search" />
+}
+
+function Checkbox() {
+
+  return <>
+    <input type="checkbox" name="isInStock" id="is-in-stock-checkbox" />
+    <label htmlFor="isInStock">Show only products in stock</label>
+  </>
+}
+
+function ProductTable() {
+  return (
+    <div className="product-table">
+      <div className="row category-types">
+        <div className="category-name">Name</div>
+        <div className="category-price">Price</div>
+      </div>
+      <ProductCategoryRow catergoryName="Fruits" />
+      <ProductRow productName="Apple" productPrice="1" />
+      <ProductRow productName="Dragonfruit" productPrice="1" />
+      <ProductRow productName="Passionfruit" productPrice="2" highestPrice/>
+      <ProductCategoryRow catergoryName="Vegetable" />
+      <ProductRow productName="Spinach" productPrice="2" />
+      <ProductRow productName="Pumpkin" productPrice="4" highestPrice/>
+      <ProductRow productName="Peas" productPrice="1" />
     </div>
+  )
+}
+
+function ProductCategoryRow({ catergoryName }) {
+  return <div className="row category-title">{catergoryName}</div>
+}
+
+function ProductRow({ productName, productPrice, highestPrice}) { 
+
+  let className = highestPrice ? 'row category-row highest-price' : 'row category-row'
+
+  return <div className={className}>
+    <div className="category-name">{productName}</div>
+    <div className="category-price">${productPrice}</div>
   </div>
 }
 
-
-
-function productTable() {
-
-  return <div className="product-table">
-    <div className="row category-types">
-      <div className="types-name">Nom</div>
-      <div className="types-price">Prix</div>
-    </div>
-    <div className="category category-fructs">
-      <div className="row category-title">Fruits</div>
-      <div className="row category-row">
-        <div className="product-name">Apple</div>
-        <div className="product-price">$1</div>
-      </div>
-      <div className="row category-row">
-        <div className="product-name">Dragonfruit</div>
-        <div className="product-price">$1</div>
-      </div>
-      <div className="row category-row highest-price">
-        <div className="product-name">Passionfruit</div>
-        <div className="product-price">$2</div>
-      </div>
-    </div>
-    <div className="category category-vegetables">
-      <div className="row category-title">Vegetables</div>
-      <div className="row category-row">
-        <div className="product-name">Spinach</div>
-        <div className="product-price">$2</div>
-      </div>
-      <div className="row category-row highest-price highest-price">
-        <div className="product-name">Pumpkin</div>
-        <div className="product-price">$4</div>
-      </div>
-      <div className="row category-row">
-        <div className="product-name">Peas</div>
-        <div className="product-price">$1</div>
-      </div>
-    </div>
-  </div>
-
-}
-
-export { searchBar as SearchBar, productTable as ProductTable }
+export { SearchBar, ProductTable }
